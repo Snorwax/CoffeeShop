30ed7a87c84dda58b6278101b9057934dd1be4a5
snorwax <mallenikhil@gmail.com>
2024-07-28 14:59:06 -0500

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+import '../styles/style.css'
+import '../styles/about.css'
+import '../styles/Online.css'
+import '../styles/Footer.css'
+import '../styles/model.css'
+
+import modelBtn from './util/model';
+
+modelBtn();
